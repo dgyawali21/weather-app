65b436f959783d7d3a0be6f2c150fc281fed2158
charliebischoff <charlie.bischoff@unh.edu>
2024-03-09 21:00:37 -0500

    Unzipped

---
+++ sandbox/src/alert.jsx
@@ -0,0 +1,45 @@
+import { Label, Input, FormGroup } from "reactstrap";
+import React from "react";
+
+const Alert = ({ dailyalerts }) => {
+  const celsiusToFahrenheit = (celsiusTemp) => {
+    return (celsiusTemp * 9) / 5 + 32;
+  };
+
+  //makes sure that the zipcode given is valid
+  try {
+    const weatherAlerts = dailyalerts
+      ? `
+Visability: ${dailyalerts.visibility} m
+Feels like: ${celsiusToFahrenheit(dailyalerts.main.feels_like).toFixed(1)} °F
+Wind Speed: ${dailyalerts.wind.speed} m/s
+`
+      : "";
+    return (
+      <FormGroup>
+        <Label for="WeatherAlerts">Weather Alerts:</Label>
+        <Input
+          id="WeatherAlerts"
+          name="text"
+          type="textarea"
+          rows="5"
+          value={weatherAlerts}
+        />
+      </FormGroup>
+    );
+  } catch (error) {
+    return (
+      <FormGroup>
+        <Label for="WeatherAlerts">Weather Alerts:</Label>
+        <Input
+          id="WeatherAlerts"
+          name="text"
+          type="textarea"
+          rows="5"
+          value="Zipcode not found"
+        />
+      </FormGroup>
+    );
+  }
+};
+export default Alert;
